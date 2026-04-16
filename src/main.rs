4a84d4edbaa81c159214854b338ea3322bc62d56
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2026-04-16 11:35:22 +0800

    feat: store values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -14,17 +16,32 @@ async fn main() {
         // 握手完成后，内核把连接放进 accept queue（已连接队列）
         // 从等待队列里取出一个已经完成握手的客户端连接，并返回一个新的 socket
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async { process(socket).await });
     }
 }
 
 async fn process(socket: TcpStream) {
-    let mut conn = Connection::new(socket);
-    if let Some(frame) = conn.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    use std::collections::HashMap;
 
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
+    use mini_redis::Command::{self, Get, Set};
+    let mut conn = Connection::new(socket);
+    let mut db = HashMap::new();
+    while let Some(frame) = conn.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => unimplemented!("{:?}", cmd),
+        };
+
         let _ = conn.write_frame(&response).await;
     }
 }
